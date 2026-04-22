6394ca08f4961c04d185151f38f7f81f08d9aaf2
John Marountas <marjohn@imbb.forth.gr>
2026-04-22 13:31:45 +0300

    fixed lint options

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
 
     @app.route("/")
-    def home():                
+    def home():
         return {"status": "ok"}
 
     return app
